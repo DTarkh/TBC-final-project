6274ab76532a130a03620ec332fb9bc2f18590a0
David Tarkhnishvili <datotarkh@gmail.com>
2024-12-22 22:26:16 +0100

    added posts list

--- app/[locale]/(dashboard)/blog/page.tsx
+++ app/[locale]/(dashboard)/blog/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+
+interface Post{
+  title: string;
+  body: string;
+  created_at: Date;
+  user_id: number;
+  id:number;
 }
 
-export default page
+interface BlogPostProps {
+  posts: Post[];
+}
+
+
+const BlogPost = async () => {
+  
+  const data = await fetch("http://localhost:3000/api/posts")
+  const posts: Post[] = await data.json()
+
+
+  return (
+  
+    <div>
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          className="max-w-2xl mx-auto my-8 p-6 bg-white rounded-lg shadow-md"
+        >
+          {/* Title */}
+          <h1 className="text-2xl font-bold text-gray-800 mb-4">{post.title}</h1>
+
+          {/* Metadata */}
+          <div className="flex justify-between items-center text-sm text-gray-500 mb-6">
+            <span>By User: {post.user_id}</span>
+            <span>{new Date(post.created_at).toLocaleDateString()}</span>
+          </div>
+
+          {/* Body */}
+          <p className="text-gray-700 leading-relaxed">{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPost;
---
+++ app/api/posts/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(req: NextRequest) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.from("posts_multilang").select("*");
+
+  if (error) {
+    console.error("Error fetching rows:", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data);
+}
