888177492f13cc3340e566b34bd57ae1265d7d9b
David Tarkhnishvili <datotarkh@gmail.com>
2024-12-23 15:27:18 +0100

    fixed bug, added link to navigate to cart page

--- app/[locale]/Components/Cart.tsx
+++ app/[locale]/Components/Cart.tsx
@@ -1,20 +1,23 @@
 import { SlHandbag } from "react-icons/sl";
 import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
 
 const Cart = () => {
-    const t = useTranslations("Navigation");
+  const t = useTranslations("Navigation");
   return (
     <div className="dropdown dropdown-end " role="button">
-      
-        <div className="flex items-center max-lg:hidden">
-          <SlHandbag className="text-4xl mx-2 text-[#14213D] outline-none hover:scale-110 transition-transform dark:text-[#E5E5E5]" tabIndex={0}/>
-          <div>
-            <p className="text-slate-500 text-sm whitespace-nowrap">
+      <div className="flex items-center max-lg:hidden">
+        <SlHandbag
+          className="text-4xl mx-2 text-[#14213D] outline-none hover:scale-110 transition-transform dark:text-[#E5E5E5]"
+          tabIndex={0}
+        />
+        <div>
+          <p className="text-slate-500 text-sm whitespace-nowrap">
             {t("cart")}
-            </p>
-            <h2 className="whitespace-nowrap text-lime-700 font-bold">$0.00</h2>
-          </div>
-       
+          </p>
+          <h2 className="whitespace-nowrap text-lime-700 font-bold">$0.00</h2>
+        </div>
+
         <span className="badge badge-sm indicator-item absolute right-[9px] top-[25px]">
           8
         </span>
@@ -28,7 +31,9 @@ const Cart = () => {
           <span className="text-lg font-bold">8 Items</span>
           <span className="text-info">Subtotal: $999</span>
           <div className="card-actions">
-            <button className="btn btn-primary btn-block">View cart</button>
+            <Link href="/cart">
+              <button className="btn btn-primary btn-block">View cart</button>
+            </Link>
           </div>
         </div>
       </div>
