dd87e4b0025c5dc91acd55947f3970ce29d155a6
David Tarkhnishvili <datotarkh@gmail.com>
2024-12-26 13:59:45 +0100

    updated cart fetching hook

--- app/[locale]/Components/Hooks/useCart.tsx
+++ app/[locale]/Components/Hooks/useCart.tsx
@@ -2,22 +2,22 @@
 
 import { useState, useEffect } from "react";
 
-interface Cart {
-  stripe_product_id: string;
+interface CartItem {
+  stripe_price_id: string;
 }
 
 const useCart = () => {
-  const [priceId, setPriceId] = useState<string | null>(null); // Initializing with a type
+  const [cart, setCart] = useState<CartItem[]>(); // Initializing with a type
 
   useEffect(() => {
-    // Log to check if useEffect is triggered
-    console.log("useEffect triggered");
+  
 
     fetch('http://localhost:3000/api/cart')
       .then(response => response.json())
       .then(cart => {
         console.log(cart);  // This logs the cart data returned from the API
-        setPriceId(cart.stripe_product_id); // Update state with fetched data (modify as needed)
+        setCart(cart); // Update state with fetched data (modify as needed)
+        
       })
       .catch(error => {
         console.error('Error fetching cart:', error);
@@ -25,7 +25,7 @@ const useCart = () => {
 
   }, []); // Empty dependency array ensures this effect runs once after the initial render
 
-  return priceId;
+  return cart
   
 }
 
