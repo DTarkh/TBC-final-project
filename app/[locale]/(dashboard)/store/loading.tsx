466b6c2e4ee30d8e50ad3b99d57ee2cd2fc5d3d8
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-03 13:47:06 +0100

    updated loading ui on store page

---
+++ app/[locale]/(dashboard)/store/loading.tsx
@@ -0,0 +1,40 @@
+import  { CategoriesList } from "./page"
+import Skeletons from "../../Components/Skeletons";
+
+const loading = () => {
+  return (
+    <div className="grid grid-flow-col gap-3 w-full px-[10%] max-xl:px-[2%] grid-cols-[250px_2fr_3fr] grid-rows-[78px_2fr_3fr] max-md:flex max-md:flex-col">
+    <div className="row-span-3  max-md:hidden">
+      <h3 className="text-2xl font-normal pb-[7px] dark:text-[#E5E5E5] pt-[28px]">
+        Browse By
+      </h3>
+      <div className="divider dark:divider-warning"></div>
+
+      <CategoriesList />
+      <div className="divider dark:divider-warning"></div>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-xl font-normal whitespace-nowrap dark:text-[#E5E5E5] ">
+          Filter By Price
+        </h3>
+        
+      </div>
+    </div>
+
+    <div className="col-span-2 ">
+      <div className="flex items-center justify-between gap-4">
+        <h2 className="text-4xl font-light dark:text-[#E5E5E5]">
+       
+        </h2>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+    <div className="row-span-2 col-span-2  grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1  gap-4">
+    <Skeletons/>
+    </div>
+  </div>
+  );
+}
+
+export default loading
--- app/[locale]/Components/Skeletons.tsx
+++ app/[locale]/Components/Skeletons.tsx
@@ -1,14 +1,12 @@
-
-
 const Skeletons = () => {
   return (
     <div className="flex w-52 flex-col gap-4">
-  <div className="skeleton h-32 w-full"></div>
-  <div className="skeleton h-4 w-28"></div>
-  <div className="skeleton h-4 w-full"></div>
-  <div className="skeleton h-4 w-full"></div>
-</div>
-  )
-}
+      <div className="skeleton h-32 w-full bg-gray-300"></div>
+      <div className="skeleton h-4 w-28 bg-gray-300"></div>
+      <div className="skeleton h-4 w-full bg-gray-300"></div>
+      <div className="skeleton h-4 w-full bg-gray-300"></div>
+    </div>
+  );
+};
 
-export default Skeletons
+export default Skeletons;
