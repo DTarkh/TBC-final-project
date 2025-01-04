9126b2554e220f0d51e0336bf9d025356420ae07
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-04 17:57:26 +0100

    changed component name

--- app/[locale]/(dashboard)/cart/page.tsx
+++ app/[locale]/(dashboard)/cart/page.tsx
@@ -24,30 +24,25 @@ interface Product {
   price: number;
 }
 
-const Page = () => {
+const CartPage = () => {
   const { cart, setCart } = useCart();
-  const [cartItemsNumber, setCartItemsNumber] = useState(0)
-  const [totalPrice, setTotalPrice] = useState(0)
-  
+  const [cartItemsNumber, setCartItemsNumber] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
   useEffect(() => {
     if (cart) {
       const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
-      const totalAmount = cart.reduce((total, item) => total + item.products.price * item.quantity,0)
+      const totalAmount = cart.reduce(
+        (total, item) => total + item.products.price * item.quantity,
+        0
+      );
       setCartItemsNumber(totalItems);
-      setTotalPrice(totalAmount)
+      setTotalPrice(totalAmount);
     } else {
       setCartItemsNumber(0);
-      setTotalPrice(0)
+      setTotalPrice(0);
     }
   }, [cart]);
-
-  // const totalAmount = cart
-  //   ? cart.reduce(
-  //       (total, item) => total + item.products.price * item.quantity,
-  //       0
-  //     )
-  //   : 0;
-
 
   const onDelete = async (productId: number) => {
     if (cart) {
@@ -76,84 +71,81 @@ const Page = () => {
       .eq("product_id", productId);
   };
 
-  
-
-
   if (!cart || cart.length === 0) {
     return <div>Your cart is empty</div>;
   }
   return (
     <div className="w-full px-[10%] py-10 mx-auto">
-       <h1 className="text-2xl font-bold text-gray-800 mb-6">
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">
         My Cart ({cartItemsNumber} items)
       </h1>
       <h1 className="text-2xl font-bold text-gray-800 mb-6">My Cart</h1>
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="text-left px-6 py-3 text-sm font-medium text-gray-700"></th>
-                <th className="text-left px-6 py-3 text-sm font-medium text-gray-700 whitespace-nowrap">
-                  Product Name
-                </th>
-                <th className="text-left px-6 py-3 text-sm font-medium text-gray-700">
-                  Quantity
-                </th>
-                <th className="text-left px-6 py-3 text-sm font-medium text-gray-700 whitespace-nowrap">
-                  Price per item
-                </th>
-                <th className="text-left px-6 py-3 text-sm font-medium text-gray-700 whitespace-nowrap">
-                  Total Price
-                </th>
-                <th className="text-left px-6 py-3 text-sm font-medium text-gray-700"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {cart.map((item: CartItem) => (
-                <tr key={item.id} className="border-t">
-                  <td className="px-6 py-4">
-                    <img
-                      src={item.products.thumbnail}
-                      alt={item.products.title_en}
-                      className="w-16 h-16 object-cover rounded-lg"
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="text-left px-6 py-3 text-sm font-medium text-gray-700"></th>
+              <th className="text-left px-6 py-3 text-sm font-medium text-gray-700 whitespace-nowrap">
+                Product Name
+              </th>
+              <th className="text-left px-6 py-3 text-sm font-medium text-gray-700">
+                Quantity
+              </th>
+              <th className="text-left px-6 py-3 text-sm font-medium text-gray-700 whitespace-nowrap">
+                Price per item
+              </th>
+              <th className="text-left px-6 py-3 text-sm font-medium text-gray-700 whitespace-nowrap">
+                Total Price
+              </th>
+              <th className="text-left px-6 py-3 text-sm font-medium text-gray-700"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item: CartItem) => (
+              <tr key={item.id} className="border-t">
+                <td className="px-6 py-4">
+                  <img
+                    src={item.products.thumbnail}
+                    alt={item.products.title_en}
+                    className="w-16 h-16 object-cover rounded-lg"
+                  />
+                </td>
+                <td className="px-6 py-4 text-gray-700">
+                  {item.products.title_en}
+                </td>
+                <td className="px-6 py-4 text-gray-700">
+                  <form className="flex gap-2">
+                    <input
+                      type="number"
+                      className="input w-[70px] border"
+                      value={item.quantity}
+                      min="1"
+                      onChange={(e) => {
+                        const newQuantity = Number(e.target.value);
+                        onUpdate(item.product_id, newQuantity);
+                      }}
                     />
-                  </td>
-                  <td className="px-6 py-4 text-gray-700">
-                    {item.products.title_en}
-                  </td>
-                  <td className="px-6 py-4 text-gray-700">
-                    <form className="flex gap-2">
-                      <input
-                        type="number"
-                        className="input w-[70px] border"
-                        value={item.quantity}
-                        min="1"
-                        onChange={(e) => {
-                          const newQuantity = Number(e.target.value);
-                          onUpdate(item.product_id, newQuantity);
-                        }}
-                      />
-                    </form>
-                  </td>
-                  <td className="px-6 py-4 text-gray-700 font-semibold ">
-                    ${item.products.price.toFixed(2)}
-                  </td>
-                  <td className="px-6 py-4 text-gray-700 font-semibold whitespace-nowrap">
-                    ${(item.products.price * item.quantity).toFixed(2)}
-                  </td>
-                  <td className="px-6 py-4">
-                    <button
-                      className="btn btn-error"
-                      onClick={() => onDelete(item.product_id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                  </form>
+                </td>
+                <td className="px-6 py-4 text-gray-700 font-semibold ">
+                  ${item.products.price.toFixed(2)}
+                </td>
+                <td className="px-6 py-4 text-gray-700 font-semibold whitespace-nowrap">
+                  ${(item.products.price * item.quantity).toFixed(2)}
+                </td>
+                <td className="px-6 py-4">
+                  <button
+                    className="btn btn-error"
+                    onClick={() => onDelete(item.product_id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div className="flex items-center justify-between p-2">
         <h2 className="text-2xl">Total Amount: ${totalPrice.toFixed(2)}</h2>
         {/* Replace with actual total */}
@@ -165,14 +157,7 @@ const Page = () => {
         </div>
       </div>
     </div>
-  )
-  
-
-    
-
-  
-
-  
+  );
 };
 
-export default Page;
+export default CartPage;
