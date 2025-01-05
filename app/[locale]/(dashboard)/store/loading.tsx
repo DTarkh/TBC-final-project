04f9dc66afe413f3e64da53b9608c4a0573b7429
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-05 08:31:23 +0100

    styles update

--- app/[locale]/(dashboard)/store/loading.tsx
+++ app/[locale]/(dashboard)/store/loading.tsx
@@ -32,6 +32,11 @@ const loading = () => {
     </div>
     <div className="row-span-2 col-span-2  grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1  gap-4">
     <Skeletons/>
+    <Skeletons/>
+    <Skeletons/>
+    <Skeletons/>
+    <Skeletons/>
+    <Skeletons/>
     </div>
   </div>
   );
--- app/[locale]/Components/Skeletons.tsx
+++ app/[locale]/Components/Skeletons.tsx
@@ -1,10 +1,11 @@
 const Skeletons = () => {
   return (
-    <div className="flex w-52 flex-col gap-4">
-      <div className="skeleton h-32 w-full bg-gray-300"></div>
-      <div className="skeleton h-4 w-28 bg-gray-300"></div>
-      <div className="skeleton h-4 w-full bg-gray-300"></div>
-      <div className="skeleton h-4 w-full bg-gray-300"></div>
+    <div className="flex w-52 flex-col gap-4 items-center">
+      <div className="skeleton h-[270px] w-[280px] bg-gray-300 rounded-none"></div>
+      <div className="skeleton h-2 w-28 bg-gray-300"></div>
+      <div className="skeleton h-3 w-full bg-gray-300"></div>
+      <div className="skeleton h-3 w-28 bg-gray-300"></div>
+      <div className="skeleton h-3 w-20 bg-gray-300"></div>
     </div>
   );
 };
