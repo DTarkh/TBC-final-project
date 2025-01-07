fae004a33b9f966b647430c34865d8c8fb220153
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-07 08:49:10 +0100

    fix bug

--- app/[locale]/Components/AddToCart.tsx
+++ app/[locale]/Components/AddToCart.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { createClient } from "@/utils/supabase/client";
@@ -48,77 +47,91 @@ const AddToCart = ({
 
     const newProduct: CartItem = {
       id: Math.floor(Math.random() * 1000000),
-      created_at:"",
+      created_at: "",
       product_id: productId,
       user_id: userId,
       stripe_product_id: "",
       stripe_price_id: "",
       quantity: 1,
-      products: { price: productPrice, title_en: productName, thumbnail: thumbnail }
+      products: {
+        price: productPrice,
+        title_en: productName,
+        thumbnail: thumbnail,
+      },
     };
 
-    
     // Check if the product exists in the cart
-    const existingCartItem = cart.find((item) => item.product_id === productId);
-
-    if (existingCartItem) {
-     
-      const updatedCart = cart.map((item) =>
-        item.product_id === productId
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
+    if (cart) {
+      const existingCartItem = cart.find(
+        (item) => item.product_id === productId
       );
-      setCart(updatedCart);
 
-      // Update cart items number and total price
-      const totalItems = updatedCart.reduce((sum, item) => sum + item.quantity, 0);
-      const totalAmount = updatedCart.reduce(
-        (total, item) => total + item.products.price * item.quantity,
-        0
-      );
-      setCartItemsNumber(totalItems);
-      setTotalPrice(totalAmount);
+      if (existingCartItem) {
+        const updatedCart = cart.map((item) =>
+          item.product_id === productId
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+        setCart(updatedCart);
 
-      
-      const { error: updateError } = await supabase
-        .from("cart")
-        .update({ quantity: existingCartItem.quantity + 1 })
-        .eq("product_id", productId)
-        .eq("user_id", userId);
+        // Update cart items number and total price
+        const totalItems = updatedCart.reduce(
+          (sum, item) => sum + item.quantity,
+          0
+        );
+        const totalAmount = updatedCart.reduce(
+          (total, item) => total + item.products.price * item.quantity,
+          0
+        );
+        setCartItemsNumber(totalItems);
+        setTotalPrice(totalAmount);
 
-      if (updateError) {
-        console.error("Error updating quantity:", updateError);
+        const { error: updateError } = await supabase
+          .from("cart")
+          .update({ quantity: existingCartItem.quantity + 1 })
+          .eq("product_id", productId)
+          .eq("user_id", userId);
+
+        if (updateError) {
+          console.error("Error updating quantity:", updateError);
+        }
+      } else {
+        const updatedCart: CartItem[] = cart
+          ? [...cart, newProduct]
+          : [newProduct];
+
+        // setCart((prevCart) => (prevCart ? [...prevCart, newProduct] : [newProduct]));
+        setCart(updatedCart);
+        // setCart(newProduct);
+        // Update cart items number and total price
+        // const updatedCart = cart ? [...cart, newProduct] : [newProduct];
+
+        const totalItems = updatedCart.reduce(
+          (sum, item) => sum + item.quantity,
+          0
+        );
+        const totalAmount = updatedCart.reduce(
+          (total, item) => total + item.products.price * item.quantity,
+          0
+        );
+        setCartItemsNumber(totalItems);
+        setTotalPrice(totalAmount);
+
+        // Insert the new product into the database
+        const { error: insertError } = await supabase.from("cart").insert({
+          product_id: productId,
+          user_id: userId,
+          stripe_product_id: "",
+          stripe_price_id: "",
+          quantity: 1,
+        });
+
+        if (insertError) {
+          console.error("Error inserting new item:", insertError);
+        }
       }
-    } else { 
-      
-      const updatedCart: CartItem[] = cart ? [...cart, newProduct] : [newProduct];
-
-      // setCart((prevCart) => (prevCart ? [...prevCart, newProduct] : [newProduct]));
-      setCart(updatedCart)
-      // setCart(newProduct);
-      // Update cart items number and total price
-      // const updatedCart = cart ? [...cart, newProduct] : [newProduct];
-
-      const totalItems = updatedCart.reduce((sum, item) => sum + item.quantity, 0);
-      const totalAmount = updatedCart.reduce(
-        (total, item) => total + item.products.price * item.quantity,
-        0
-      );
-      setCartItemsNumber(totalItems);
-      setTotalPrice(totalAmount);
-
-      // Insert the new product into the database
-      const { error: insertError } = await supabase.from("cart").insert({
-        product_id: productId,
-        user_id: userId,
-        stripe_product_id: "",
-        stripe_price_id: "",
-        quantity: 1,
-      });
-
-      if (insertError) {
-        console.error("Error inserting new item:", insertError);
-      }
+    } else {
+      <div><h2>Cart doesnt exist!</h2></div>
     }
   }
 
