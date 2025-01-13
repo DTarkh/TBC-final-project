15433c27047b75769c0459a06f38c4d4d7ecbe25
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-13 18:37:46 +0400

    added skeleton for the category list

--- app/[locale]/Components/Skeletons.tsx
+++ app/[locale]/Components/Skeletons.tsx
@@ -1,7 +1,7 @@
 const Skeletons = () => {
   return (
     <div className="flex w-52 flex-col gap-4 items-center">
-      <div className="skeleton h-[270px] w-[280px] bg-gray-300 rounded-none"></div>
+      <div className="skeleton h-[270px] w-[270px] bg-gray-300 rounded-none"></div>
       <div className="skeleton h-2 w-28 bg-gray-300"></div>
       <div className="skeleton h-3 w-full bg-gray-300"></div>
       <div className="skeleton h-3 w-28 bg-gray-300"></div>
