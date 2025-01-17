e8d3bc6096d4a2b88d6477972bad4244ada97d2c
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-17 16:06:30 +0400

    refactor menu and updated styles

--- app/[locale]/Components/NavAdd.tsx
+++ app/[locale]/Components/NavAdd.tsx
@@ -17,7 +17,7 @@ const NavAdd = () => {
   // const router = useRouter();
   // const [query, setQuery] = useState<string>("");
   const t = useTranslations("Navigation");
-  const [isVisible, setIsVisible] = useState(false)
+  const [isVisible, setIsVisible] = useState(false);
 
   // const handleClick = () => {
   //   if (query.length > 0) {
@@ -29,12 +29,11 @@ const NavAdd = () => {
   //   setQuery("");
   // };
 
-  const onBurgerClick =() =>{
-
-    setIsVisible(!isVisible)
-  }
-   // Add/remove `no-scroll` class when `isVisible` changes
-   useEffect(() => {
+  const onBurgerClick = () => {
+    setIsVisible(!isVisible);
+  };
+  // Add/remove `no-scroll` class when `isVisible` changes
+  useEffect(() => {
     if (isVisible) {
       document.body.classList.add("no-scroll");
     } else {
@@ -49,18 +48,22 @@ const NavAdd = () => {
 
   return (
     <nav className="w-full flex  space-x-6 items-center max-sm:px-[2%]  px-[10%] py-2 justify-between bg-[#E5E5E5] dark:bg-[#14213D]">
-      {isVisible && <Menu setIsVisible={setIsVisible}/>}
-      <div className="w-full flex gap-2 items-center">
-      <GiHamburgerMenu className="text-3xl lg:hidden font-bold" onClick={onBurgerClick}/>
-        <Link
-          href="/home"
-          className="flex items-center gap-1 font-bold text-2xl text-[#14213D] dark:text-[#E5E5E5]"
-        >
-          <GiHolosphere className="text-3xl text-red-700" />
-          SPHERE.
-        </Link>
+      {isVisible && <Menu setIsVisible={setIsVisible} />}
+      <div className="relative">
+        <div className="flex gap-2 items-center">
+          <GiHamburgerMenu
+            className="text-3xl lg:hidden font-bold"
+            onClick={onBurgerClick}
+          />
+          <Link
+            href="/home"
+            className="flex items-center gap-1 font-bold text-2xl text-[#14213D] dark:text-[#E5E5E5]"
+          >
+            <GiHolosphere className="text-3xl text-red-700" />
+            SPHERE.
+          </Link>
 
-        {/* <div className="relative w-full max-w-full">
+          {/* <div className="relative w-full max-w-full">
           <span className="absolute top-[10px] right-3 flex items-center text-gray-500">
             <CiSearch
               size={27}
@@ -77,8 +80,10 @@ const NavAdd = () => {
             className="input w-full max-w-full max-lg:hidden border-[#14213D]"
           />
         </div> */}
-        <Search />
+        </div>
+        <p className="absolute top-5 right-1 text-zinc-600">eCommerce</p>
       </div>
+      <Search />
       <div className="flex items-center gap-2 justify-between">
         <Profile />
         <Cart />
@@ -90,59 +95,46 @@ const NavAdd = () => {
 
 export default NavAdd;
 
-
-const Menu = ({ setIsVisible }: { setIsVisible: (isVisible: boolean) => void }) => {
+const Menu = ({
+  setIsVisible,
+}: {
+  setIsVisible: (isVisible: boolean) => void;
+}) => {
   const t = useTranslations("Footer");
+
+  const MenuItems = [
+    { link: "/home", text: t("home") },
+    { link: "/store", text: t("store") },
+    { link: "/blog", text: t("blog") },
+    { link: "/about", text: t("about") },
+    { link: "/contact", text: t("contact") },
+  ];
+
   const onClose = () => {
     setIsVisible(false);
   };
 
   return (
-    <div className="bg-red-600 w-full h-[100vh] absolute z-20 left-0 top-0 lg:hidden p-11 flex flex-col items-center">
+    <div className="bg-[#E5E5E5] w-full h-[100vh] absolute z-20 left-0 top-0 lg:hidden pt-20 flex flex-col items-center">
       <IoIosCloseCircle
-        className="absolute top-3 right-7 text-2xl cursor-pointer"
+        className="absolute top-3 right-7 text-2xl cursor-pointer text-[#FCA311]"
         onClick={onClose}
       />
-      <h3 className="dark:text-[#FFFFFF] text-[#14213D] font-bold pb-6 max-md:pb-4">
-            {t("menu")}
-          </h3>
-          <ul className="flex flex-col">
-            <Link
-              href="/home"
-              className="dark:text-[#FFFFFF] text-[#14213D] pb-5 max-md:pb-2"
-              onClick={onClose}
-            >
-              {t("home")}
-            </Link>
-            <Link
-              href="/store"
-              className="dark:text-[#FFFFFF] text-[#14213D] pb-5 max-md:pb-2"
-              onClick={onClose}
-            >
-              {t("store")}
-            </Link>
-            <Link
-              href="/blog"
-              className="dark:text-[#FFFFFF] text-[#14213D] pb-5 max-md:pb-2"
-              onClick={onClose}
-            >
-              {t("blog")}
-            </Link>
-            <Link
-              href="/about"
-              className="dark:text-[#FFFFFF] text-[#14213D] pb-5 max-md:pb-2"
-              onClick={onClose}
-            >
-              {t("about")}
-            </Link>
-            <Link
-              href="/contact"
-              className="dark:text-[#FFFFFF] text-[#14213D] pb-5 max-md:pb-2"
-              onClick={onClose}
-            >
-              {t("contact")}
-            </Link>
-          </ul>
+      <h3 className="text-[#14213D] text-4xl font-bold  pb-10">
+        {t("menu")}
+      </h3>
+      {MenuItems.map((menuItem, index) => (
+        <div className="flex flex-col items-center w-full" key={index}>
+          <Link
+            href={menuItem.link}
+            className="text-[#14213D] pb-5 max-md:pb-2 text-3xl hover:underline"
+            onClick={onClose}
+          >
+            {menuItem.text}
+          </Link>
+          <div className="w-full h-[1px] bg-[#FCA311] my-2"></div>
+        </div>
+      ))}
     </div>
   );
 };
