8a995de5d73e5e2746739c60291e1155cb32388c
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-21 16:37:18 +0400

    add cart empty functionality on logout

--- app/[locale]/Components/Profile.tsx
+++ app/[locale]/Components/Profile.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation"; // For client-side routing
-import useUser from "../Components//Hooks/useUser"
+import useUser from "../Components//Hooks/useUser";
 
 interface User {
   id: string;
@@ -12,20 +12,34 @@ interface User {
 }
 
 const Profile = () => {
-  const { user, loading } = useUser()
+  const { user, loading } = useUser();
   const supabase = createClient();
   const router = useRouter(); // Next.js router for client-side navigation
   const t = useTranslations("Navigation");
 
-
   const handleSignOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Error signing out:", error.message);
-      return;
+    try {
+      const { error: deleteError } = await supabase
+        .from("cart")
+        .delete()
+        .eq("user_id", user?.id);
+      if (deleteError) {
+        console.error("Error deleting cart:", deleteError.message);
+      } else {
+        console.log("Cart deleted successfully for user ID:", user?.id);
+      }
+
+      const { error: signOutError } = await supabase.auth.signOut();
+      if (signOutError) {
+        console.error("Error signing out:", signOutError.message);
+      } else {
+        console.log("User signed out successfully");
+      }
+
+      router.push("/en/sign-in");
+    } catch (err) {
+      console.error("Unexpected error during sign-out process:", err);
     }
-    // Redirect to the sign-in page
-    router.push("/en/sign-in");
   };
 
   return (
@@ -36,16 +50,26 @@ const Profile = () => {
           tabIndex={user ? 0 : undefined}
         />
         <div>
-          <p className="text-[#14213D] text-sm dark:text-[#E5E5E5]">{t("welcome")}</p>
+          <p className="text-[#14213D] text-sm dark:text-[#E5E5E5]">
+            {t("welcome")}
+          </p>
           <h2 className="whitespace-nowrap text-[#14213D] dark:text-[#E5E5E5]">
-          {loading ? "Loading..." : user ? user.email : <Link href="/sign-in">Sign-in/Sign-up</Link>}
+            {loading ? (
+              "Loading..."
+            ) : user ? (
+              user.email
+            ) : (
+              <Link href="/sign-in">Sign-in/Sign-up</Link>
+            )}
           </h2>
         </div>
       </div>
 
       <ul
         tabIndex={user ? 0 : undefined}
-        className={`menu menu-sm dropdown-content bg-base-100 rounded-box z-40 mt-3 w-52 p-2 shadow ${!user ? "hidden" : ""}`}
+        className={`menu menu-sm dropdown-content bg-base-100 rounded-box z-40 mt-3 w-52 p-2 shadow ${
+          !user ? "hidden" : ""
+        }`}
       >
         <li>
           <Link href="/profile">My Profile</Link>
