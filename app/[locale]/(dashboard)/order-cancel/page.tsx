d24e8a53be12208c8e85569792b5eeed25abcc9f
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-22 10:25:41 +0400

    updated stripe cancel page

--- app/[locale]/(dashboard)/order-cancel/page.tsx
+++ app/[locale]/(dashboard)/order-cancel/page.tsx
@@ -1,15 +1,16 @@
+import { Link } from "@/i18n/routing";
 import Head from "next/head";
-
-
 
 const Error = () => {
   return (
     <>
-      <Head>
-        <title>Error Page</title>
-      </Head>
-      <div className="h-screen flex justify-center items-center">
-        <h1 className="text-white text-4xl">Error</h1>
+      <div className="flex min-w-screen flex-col items-center justify-between gap-2">
+        <h1 className="mt-[20rem] scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          Payment Cancelled 😢
+        </h1>
+        <p className="leading-7">The good news is, you can try again 😊</p>
+        <button className="btn px-20"><Link href="/store">Return</Link></button>
+        
       </div>
     </>
   );
