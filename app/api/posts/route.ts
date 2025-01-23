9a295b09e4b7ae72c33a5426a8ff803373f5256e
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-23 12:44:21 +0400

    created api for blog posts search

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,8 +3,17 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function GET(req: NextRequest) {
   const supabase = await createClient();
+  const url = new URL(req.url);
+  const searchQuery = url.searchParams.get("blogSearch");
 
-  const { data, error } = await supabase.from("posts").select("*");
+  let query = supabase.from("posts").select("*");
+
+
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`);
+  }
+
+  const { data, error } = await query;
 
   if (error) {
     console.error("Error fetching rows:", error.message);
