39a42e12dd9cab9fa185b40258c5929bafdf2e80
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-24 10:34:42 +0400

    updated blogsection styles

--- app/[locale]/Components/BlogSection.tsx
+++ app/[locale]/Components/BlogSection.tsx
@@ -8,7 +8,7 @@ const BlogSection = async () => {
   const posts: Post[] = await data.json();
 
   const reversedPosts = posts.reverse();
-  const latestPosts = reversedPosts.slice(0, 4);
+  const latestPosts = reversedPosts.slice(0, 3);
 
 
    const supabase = await createClient()
@@ -16,11 +16,11 @@ const BlogSection = async () => {
 
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 px-[10%] max-lg:px-[2%] py-3 gap-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  px-[10%] max-lg:px-[2%] py-3 gap-2">
       {latestPosts.map((post) => (
-        <div key={post.id} className="p-6 bg-white rounded-lg shadow-md">
+        <div key={post.id} className="p-6 bg-white rounded-lg hover:shadow-md transition-all">
           <div className="flex justify-between items-center text-sm text-gray-500 mb-6">
-            <span>By User: {post.user_id}</span>
+            <span className="badge">By User: {post.user_email}</span>
             <span>
                 {new Intl.DateTimeFormat("en-US", {
                   timeZone: 'Asia/Tbilisi',
@@ -35,7 +35,7 @@ const BlogSection = async () => {
           </div>
 
           <Link href={`/blog/${post.id}`}>
-            <h1 className="text-2xl font-bold text-gray-800 mb-4">
+            <h1 className="text-2xl font-bold text-gray-800 mb-4 hover:text-[#FCA311]">
               {post.title}
             </h1>
           </Link>
