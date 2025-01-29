3966ccc71523bc0f0a457a153ac6ae9ffadf5e1e
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-30 02:28:25 +0400

    add images to categories

--- app/[locale]/(dashboard)/store/page.tsx
+++ app/[locale]/(dashboard)/store/page.tsx
@@ -5,35 +5,53 @@ import RangeSlider from "../../Components/RangeSlider";
 import Selector from "@/app/[locale]/Components/Selector";
 import ClearBtn from "../../Components/ClearBtn";
 import Pagination from "../../Components/Pagination";
-
-
+import Image from "next/image";
 
 interface Props {
   searchParams: any;
 }
 
 const categories = [
-  
-  // "Beauty and Personal Care",
-  // "Lagguage and Travel Gear",
-  // "Clothing and Accessories",
-  // "Musical Instruments",
-  // "Garden and Outdoor",
-  "Home Appliances",
-  // "Offuce Products",
-  // "Toys and Hobbies",
-  // "Smart Home",
-  "Cell Phones",
-  "Electronics",
-  "Furniture",
-  "Bedroom",
-  "Outdoor",
-  "Kitchen",
-  "Sports",
-  "Books",
-  "Tools",
-  // "Baby",
-  // "Pet",
+  {
+    category: "Home Appliances",
+    image: "https://i.postimg.cc/MZyKD9wB/Generated-Image-5.jpg"
+  },
+  {
+    category: "Cell Phones",
+    image: "https://i.postimg.cc/P5cPmGWL/WhatsApp_Image_2025-01-16_at_23.50.47.jpg"
+  },
+  {
+    category: "Electronics",
+    image: "https://i.postimg.cc/mg6bDqYv/Generated-Image-6.jpg"
+  },
+  {
+    category: "Furniture",
+    image: "https://i.postimg.cc/6qwKgNKv/Generated-Image-7.jpg"
+  },
+  {
+    category:"Bedroom",
+    image: "https://i.postimg.cc/W1tvQnBX/Generated-Image-8.jpg"
+  },
+  {
+    category: "Outdoor",
+    image: "https://i.postimg.cc/TPDXPPYb/Generated-Image-9.jpg"
+  },
+  {
+    category: "Kitchen",
+    image: "https://i.postimg.cc/6p8K0FNf/knife.webp"
+  },
+  {
+    category:  "Sports",
+    image: "https://i.postimg.cc/8CbZrPnK/shoes.webp"
+  },
+  {
+    category: "Books",
+    image: "https://i.postimg.cc/gj6hZY52/WhatsApp_Image_2025-01-16_at_23.37.34.jpg"
+  },
+  {
+    category: "Tools",
+    image: "https://i.postimg.cc/ZRnKjmTK/Generated-Image-4.jpg"
+  }
 ];
 
 const Store = async ({ searchParams }: Props) => {
@@ -119,13 +137,20 @@ const CategoriesList = () => {
     <>
       {categories.map((category, index) => (
         <div className="flex items-center gap-2" key={index}>
-          <div className="rounded-full w-[15px] h-[15px] overflow-hidden cursor-pointer bg-[#14213D] dark:bg-[#E5E5E5]"></div>
+          <Image
+            src={category.image}
+            width={30}
+            height={30}
+            alt="Generated Image"
+            className="rounded-xl m-1"
+          />
+
           <Link
-            href={`store?category=${category}`}
+            href={`store?category=${category.category}`}
             key={index}
             className="flex flex-col text-1xl dark:text-[#E5E5E5] hover:underline whitespace-nowrap"
           >
-            {category}
+            {category.category}
           </Link>
         </div>
       ))}
