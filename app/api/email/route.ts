be3c78961191ec9dc3f94cdcfd81244e6b94c494
David Tarkhnishvili <datotarkh@gmail.com>
2025-01-31 22:50:05 +0400

    created endpoint for receiving data from contact form via resend

---
+++ app/api/email/route.ts
@@ -0,0 +1,25 @@
+import { EmailTemplate } from "../../[locale]/Components/Email-template";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  const { name, email, message } = await request.json();
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "datotarkh@gmail.com",
+      subject: "Hello world",
+      react: EmailTemplate({ name, email, message }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
