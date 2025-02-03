63ffb74f805c51563a379ea76d90b606e8560b68
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-03 17:08:54 +0400

    add subscriuption card with two plans and links

--- app/[locale]/(dashboard)/subscription/page.tsx
+++ app/[locale]/(dashboard)/subscription/page.tsx
@@ -1,27 +1,120 @@
-import { FaRegCheckCircle } from "react-icons/fa";
-import Header from "../../Components/Header";
-import GetStarted from "../../Components/GetStarted";
+"use client";
 
-const SubscriptionPage = () => {
+import React, { useState } from "react";
+import useUser from "../../Components/Hooks/useUser";
+
+export const plans = [
+  {
+    link: "https://buy.stripe.com/test_cN29DD3lWbuA9BC6oM",
+    priceId: "price_1Qjx0tE1EFQDyA1CWA75IoGL",
+    price: 14.99,
+    duration: "/month",
+  },
+  {
+    link: "https://buy.stripe.com/test_bIY1776y87ek9BC5kJ",
+    priceId: "price_1QoOk3E1EFQDyA1C2w19d8sD",
+    price: 59.0,
+    duration: "/year",
+  },
+];
+
+const Pricing = () => {
+  const user = useUser();
+  const [plan, setPlan] = useState(plans[0]);
+
   return (
     <>
-    <Header>Pricing Plans</Header>
-    <div className=" w-[400px] h-[50vh] p-7 flex flex-col justify-between rounded-lg bg-white">
-      <div className="flex justify-between">
-        <p className="text-xl font-semibold">Basic</p>
-        <span className="badge">Save 20$</span>
-      </div>
-      <h1>$20/month</h1>
-      <p>Desctiption text Lorem ipsum dolor sit amet.</p>
-      <div className="flex flex-col gap-2">
-        <p className="flex items-center gap-1 "><FaRegCheckCircle className="text-green-500"/>Example feature 1</p>
-        <p className="flex items-center gap-1 "><FaRegCheckCircle className="text-green-500"/>Example feature 2</p>
-        <p className="flex items-center gap-1 "><FaRegCheckCircle className="text-green-500"/>Example feature 3</p>
-      </div>
-      <GetStarted />
-    </div>
+      <section id="pricing">
+        <div className="py-24 px-8 max-w-5xl mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <p className="font-medium text-primary mb-5">Pricing</p>
+            <h2 className="font-bold text-3xl lg:text-5xl tracking-tight">
+              Hello YouTube
+            </h2>
+          </div>
+
+          <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
+            <div className=" w-full max-w-lg">
+              <div className="relative flex flex-col h-full gap-5 lg:gap-8 z-10 bg-base-100 p-8 rounded-xl">
+                <div className="flex items-center gap-8">
+                  <div className="flex items-center gap-2">
+                    <input
+                      placeholder="monthly plan"
+                      type="radio"
+                      name="monthly"
+                      className="radio"
+                      checked={plan.price === 14.99}
+                      onChange={() => setPlan(plans[0])}
+                    />
+                    <span>Pay monthly</span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <input
+                      placeholder="yearly plan"
+                      type="radio"
+                      name="yearly"
+                      className="radio"
+                      checked={plan.price === 59.0}
+                      onChange={() => setPlan(plans[1])}
+                    />
+                    <span>Pay yearly (60% OFF 💰)</span>
+                  </div>
+                </div>
+
+                <div className="flex gap-2">
+                  <p className={`text-5xl tracking-tight font-extrabold`}>
+                    ${plan.price}
+                  </p>
+                  <div className="flex flex-col justify-end mb-[4px]">
+                    <p className="text-sm tracking-wide text-base-content/80 uppercase font-semibold">
+                      {plan.duration}
+                    </p>
+                  </div>
+                </div>
+
+                <ul className="space-y-2.5 leading-relaxed text-base flex-1">
+                  {[
+                    { name: "Fast 3-5 days delivery 🚀" },
+                    { name: "No shipping fees on all orders 🆓" },
+                    { name: "Priority order processing ✅" },
+                    { name: "Exclusive member discounts 💰" },
+                    { name: "Real-time order tracking 📦" },
+                    { name: "Dedicated 24/7 customer support ☎️" },
+                  ].map((feature, i) => (
+                    <li key={i} className="flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-[18px] h-[18px] opacity-80 shrink-0"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+
+                      <span>{feature.name} </span>
+                    </li>
+                  ))}
+                </ul>
+                <div className="space-y-2">
+                  <a
+                    className="btn btn-primary btn-block "
+                    target="_blank"
+                    href={plan.link + "?prefilled_email=" + user.user?.email}
+                  >
+                    Subscribe
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default SubscriptionPage
+export default Pricing;
