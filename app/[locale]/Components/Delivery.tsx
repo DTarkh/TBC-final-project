d4a9d7b3cc2a8dc9dd6f5ade2e4c8b8ce8833d2f
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-03 18:58:25 +0400

    Created component for delivery information

---
+++ app/[locale]/Components/Delivery.tsx
@@ -0,0 +1,22 @@
+import useUsersS from "../Components/Hooks/useUserS";
+import { User } from "../Components/Hooks/useUserS";
+import useUser from "../Components/Hooks/useUser";
+
+const Delivery = () => {
+  const { users, loading, error } = useUsersS();
+  const loggedInUser = useUser();
+  const currentUser = users?.filter(
+    (user) => user.user_id === loggedInUser.user?.id
+  );
+  return (
+    <div>
+      {currentUser?.map((user: User) => (
+        <p>
+          {user.shipping_address} {user.issubscribed}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Delivery;
