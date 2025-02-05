0f998bb601aadc603c9710f1e9d658b1557002ab
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-05 20:47:25 +0400

    Add bottom padding to Blog section

--- app/[locale]/Components/BlogSection.tsx
+++ app/[locale]/Components/BlogSection.tsx
@@ -1,7 +1,6 @@
 import { Link } from "@/i18n/routing";
 import { Post } from "../(dashboard)/blog/page";
-import { truncateText } from "@/utils/helper-functions"
-import { createClient } from "@/utils/supabase/server";
+import { truncateText } from "@/utils/helper-functions";
 
 const BlogSection = async () => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`);
@@ -10,28 +9,28 @@ const BlogSection = async () => {
   const reversedPosts = posts.reverse();
   const latestPosts = reversedPosts.slice(0, 3);
 
-
-   const supabase = await createClient()
-  
-
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  px-[10%] max-lg:px-[2%] py-3 gap-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  px-[10%] max-lg:px-[2%] pt-3 gap-2 pb-20">
       {latestPosts.map((post) => (
-        <div key={post.id} className="p-6 bg-white rounded-lg hover:shadow-md transition-all">
+        <div
+          key={post.id}
+          className="p-6 bg-white rounded-lg hover:shadow-md transition-all"
+        >
           <div className="flex justify-between items-center text-sm text-gray-500 mb-6 max-2xl:flex-col max-2xl:items-start gap-2">
-            <span className="badge whitespace-nowrap">By User: {post.user_email}</span>
             <span className="badge whitespace-nowrap">
-                {new Intl.DateTimeFormat("en-US", {
-                  timeZone: 'Asia/Tbilisi',
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: false,
-                }).format(new Date(post.created_at))}
-              </span>
+              By User: {post.user_email}
+            </span>
+            <span className="badge whitespace-nowrap">
+              {new Intl.DateTimeFormat("en-US", {
+                timeZone: "Asia/Tbilisi",
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: false,
+              }).format(new Date(post.created_at))}
+            </span>
           </div>
 
           <Link href={`/blog/${post.id}`}>
@@ -39,7 +38,9 @@ const BlogSection = async () => {
               {post.title}
             </h1>
           </Link>
-          <p className="text-gray-700 leading-relaxed">{truncateText (post.body, 30)}</p>
+          <p className="text-gray-700 leading-relaxed">
+            {truncateText(post.body, 30)}
+          </p>
         </div>
       ))}
     </div>
