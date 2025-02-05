fadf736000d70aab4f63fa63d395ff08c408433b
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-05 20:47:07 +0400

    Added text size and spacing to Header component

--- app/[locale]/Components/Header.tsx
+++ app/[locale]/Components/Header.tsx
@@ -9,7 +9,7 @@ interface Props{
 const Header = ({children}: Props) => {
   return (
     <div className="w-full px-[10%] max-lg:px-[2%] py-5">
-        <h1 className="text-2xl font-semibold text-[#14213D] dark:text-[#E5E5E5]">{children}</h1>
+        <h1 className="text-3xl font-semibold text-[#14213D] dark:text-[#E5E5E5] tracking-wide">{children}</h1>
     </div>
   )
 }
