d5b84feb0a2ab78527d42e615b98b2e1b256e7f0
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-05 21:37:26 +0400

    Changed Header component to accepting additional prop

--- app/[locale]/Components/Header.tsx
+++ app/[locale]/Components/Header.tsx
@@ -1,17 +1,21 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
-
-
-interface Props{
-    children: ReactNode
+interface Props {
+  children: ReactNode;
+  subHeader: string;
 }
 
-const Header = ({children}: Props) => {
+const Header = ({ children, subHeader }: Props) => {
   return (
-    <div className="w-full px-[10%] max-lg:px-[2%] py-5">
-        <h1 className="text-3xl font-semibold text-[#14213D] dark:text-[#E5E5E5] tracking-wide">{children}</h1>
+    <div className="w-full px-[10%] max-lg:px-[2%] pt-10 pb-5">
+      <h5 className="text-sm font-semibold text-[#FCA311] dark:text-[#E5E5E5] text-center">
+        {subHeader}
+      </h5>
+      <h1 className="text-3xl font-semibold text-[#14213D] dark:text-[#E5E5E5] tracking-wide text-center">
+        {children}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
