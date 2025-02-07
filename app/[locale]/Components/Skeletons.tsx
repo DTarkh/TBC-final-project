ae162073fdaba09bf8a47ed3b2e994523daf25b6
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-07 15:22:53 +0400

    Updated Skeletons to match actual size of the products

--- app/[locale]/Components/Skeletons.tsx
+++ app/[locale]/Components/Skeletons.tsx
@@ -1,11 +1,10 @@
 const Skeletons = () => {
   return (
-    <div className="flex w-52 flex-col gap-4 items-center">
-      <div className="skeleton h-[270px] w-[270px] bg-gray-300 rounded-none"></div>
-      <div className="skeleton h-2 w-28 bg-gray-300"></div>
-      <div className="skeleton h-3 w-full bg-gray-300"></div>
-      <div className="skeleton h-3 w-28 bg-gray-300"></div>
-      <div className="skeleton h-3 w-20 bg-gray-300"></div>
+    <div className="flex flex-col gap-4 w-full items-center">
+      <div className="skeleton h-[300px] w-full bg-gray-300 rounded-lg"></div> 
+      <div className="skeleton h-4 w-3/4 bg-gray-300"></div>
+      <div className="skeleton h-4 w-1/2 bg-gray-300"></div>
+      <div className="skeleton h-3 w-1/3 bg-gray-300"></div>
     </div>
   );
 };
