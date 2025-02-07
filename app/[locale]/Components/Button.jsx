16530d194d5eff542742a263dbecc4b6cccc45f2
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-07 19:50:16 +0400

    Updated button Component

--- app/[locale]/Components/Button.jsx
+++ app/[locale]/Components/Button.jsx
@@ -1,12 +1,14 @@
 import { Link } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
+import { FaArrowRight } from "react-icons/fa";
 
 const Button = ({ href, name }) => {
     const t = useTranslations("HomePage");
   return (
 
-    <button className="inline-block px-12 py-4 font-medium  whitespace-nowrap text-[#14213D] dark:hover:text-[#14213D]  bg-[#FCA311] hover:bg-[#ff9900] dark:bg-[#ff9900] dark:hover:bg-[#E5E5E5] dark:text-[#FFFFFF] transition-colors rounded-3xl">
+    <button className="flex px-12 py-4 font-medium text-lg whitespace-nowrap  dark:hover:text-[#14213D]  bg-[#FCA311] hover:bg-[#ff9900] dark:bg-[#ff9900] dark:hover:bg-[#E5E5E5] text-[#FFFFFF]  rounded-xl hover:scale-110 transition-transform  items-center gap-2">
     <Link href={href}>{t("button")}</Link>
+    <FaArrowRight />
     </button>
     
   );
