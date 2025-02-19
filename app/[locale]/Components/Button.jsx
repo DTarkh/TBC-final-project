f580c37742c8449cd091c0de773db40bf3e17183
David Tarkhnishvili <datotarkh@gmail.com>
2025-02-19 09:30:03 +0400

    Updated button animation duration

--- app/[locale]/Components/Button.jsx
+++ app/[locale]/Components/Button.jsx
@@ -6,9 +6,9 @@ const Button = ({ href, name }) => {
     const t = useTranslations("HomePage");
 
     return (
-        <button className="group flex px-12 py-4 font-medium text-lg whitespace-nowrap dark:hover:text-[#14213D] bg-[#FCA311] hover:bg-[#ff9900] dark:bg-[#ff9900] dark:hover:bg-[#E5E5E5] text-[#FFFFFF] rounded-xl hover:scale-110 transition-all duration-200 items-center gap-2 drop-shadow-md">
-            <Link href={href} className="group-hover:scale-110 transition duration-200 text-lg">{t("button")}</Link>
-            <FaArrowRightLong className="group-hover:scale-110 group-hover:ml-4 transition-all duration-200 font-bold"/>
+        <button className="group flex px-12 py-4 font-medium text-lg whitespace-nowrap dark:hover:text-[#14213D] bg-[#FCA311] hover:bg-[#ff9900] dark:bg-[#ff9900] dark:hover:bg-[#E5E5E5] text-[#FFFFFF] rounded-xl hover:scale-105 transition-all duration-300 items-center gap-2 drop-shadow-md">
+            <Link href={href} className="group-hover:scale-105 transition duration-500 text-lg">{t("button")}</Link>
+            <FaArrowRightLong className="group-hover:scale-105 group-hover:ml-3 transition-all duration-300 font-bold"/>
         </button>
     );
 };
