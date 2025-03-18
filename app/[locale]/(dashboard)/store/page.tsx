31bfe970a3a31d2544e0bc960b38a0b62d00939a
David Tarkhnishvili <datotarkh@gmail.com>
2025-03-18 13:51:55 +0100

    Reinstall dependencies, remove unused code

--- app/[locale]/(dashboard)/store/page.tsx
+++ app/[locale]/(dashboard)/store/page.tsx
@@ -5,22 +5,12 @@ import {
   ProductCard,
   PricePicker,
   Selector,
-  CategoriesList
+  CategoriesList,
 } from "@/app/[locale]/Components";
 import { Products } from "../../Components/Hooks/useProducts";
 
 interface Props {
-  searchParams: ProductsQuery
-}
-
-interface ProductsQuery {
-  category: string;
-  minPrice: string;
-  maxPrice: string;
-  search: string;
-  order: string;
-  per_page: number;
-  page: string;
+  searchParams: any;
 }
 
 const Store = async ({ searchParams }: Props) => {
@@ -35,23 +25,22 @@ const Store = async ({ searchParams }: Props) => {
 
   const page = parseInt(searchParams.page) || 1;
 
-  console.log("category:", category, minPrice, maxPrice, order, page, per_page);
   const start = (Number(page) - 1) * Number(per_page);
   const end = start + Number(per_page);
 
   const queryParams = new URLSearchParams();
 
-if (category) queryParams.append("category", category);
-if (minPrice) queryParams.append("minPrice", String(minPrice));
-if (maxPrice !== Infinity) queryParams.append("maxPrice", String(maxPrice));
-if (search) queryParams.append("search", search);
-if (order) queryParams.append("order", order);
-queryParams.append("per_page", String(per_page));
-queryParams.append("page", String(page));
+  if (category) queryParams.append("category", category);
+  if (minPrice) queryParams.append("minPrice", String(minPrice));
+  if (maxPrice !== Infinity) queryParams.append("maxPrice", String(maxPrice));
+  if (search) queryParams.append("search", search);
+  if (order) queryParams.append("order", order);
+  queryParams.append("per_page", String(per_page));
+  queryParams.append("page", String(page));
 
 const url = `${process.env.NEXT_PUBLIC_API_URL}/api/products?${queryParams.toString()}`;
 
-const response = await fetch(url);
+  const response = await fetch(url);
 
   const data = await response.json();
   const products: Products[] = data.slice(start, end);
@@ -66,7 +55,7 @@ const response = await fetch(url);
             Browse By
           </h3>
           <div className="h-[2px] w-[250px] bg-[#14213D] my-[24px] dark:bg-[#E5E5E5]"></div>
-          <CategoriesList/>
+          <CategoriesList />
           <div className="h-[2px] w-[250px] bg-[#14213D] my-[23px] dark:bg-[#E5E5E5]"></div>
           <div className="flex flex-col gap-3">
             <h3 className="text-xl font-normal whitespace-nowrap dark:text-[#E5E5E5] ">
@@ -104,4 +93,3 @@ const response = await fetch(url);
 };
 
 export default Store;
-
