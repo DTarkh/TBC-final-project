0dc2b521edcac671fe3bc125acbe4ab917ec983b
David Tarkhnishvili <datotarkh@gmail.com>
2025-03-18 13:18:34 +0100

    Changed initial page to home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  console.log("Redirecting to en/sign-in");
-  redirect('en/sign-in');
+  redirect('en/home');
 }
