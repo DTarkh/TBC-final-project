95c110f77bee99f09fd7c9456879d265857c0075
David Tarkhnishvili <datotarkh@gmail.com>
2025-03-18 14:41:02 +0100

    Fiex rating component

--- app/[locale]/Components/Rating.tsx
+++ app/[locale]/Components/Rating.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+
 
 const Rating = () => {
   return (
     <div className="rating">
-      <div className="mask mask-star" aria-label="1 star"></div>
-      <div className="mask mask-star" aria-label="2 star"></div>
-      <div className="mask mask-star" aria-label="3 star"></div>
-      <div className="mask mask-star" aria-label="4 star" aria-current="true"></div>
-      <div className="mask mask-star" aria-label="5 star"></div>
-    </div>
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="1 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="2 star" defaultChecked />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="3 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="4 star" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" aria-label="5 star" />
+</div>
   );
 };
 
