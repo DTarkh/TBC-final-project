822056857ba033ac3359acd34394c6e8797941a2
David Tarkhnishvili <datotarkh@gmail.com>
2025-03-18 09:31:52 +0100

    Changed name category to label

--- app/[locale]/(dashboard)/store/Categories.tsx
+++ app/[locale]/(dashboard)/store/Categories.tsx
@@ -1,44 +1,44 @@
  export const categories = [
   {
-    category: "Home Appliances",
+    label: "Home Appliances",
     image: "https://i.postimg.cc/MZyKD9wB/Generated-Image-5.jpg",
   },
   {
-    category: "Cell Phones",
+    label: "Cell Phones",
     image:
       "https://i.postimg.cc/P5cPmGWL/WhatsApp_Image_2025-01-16_at_23.50.47.jpg",
   },
   {
-    category: "Electronics",
+    label: "Electronics",
     image: "https://i.postimg.cc/mg6bDqYv/Generated-Image-6.jpg",
   },
   {
-    category: "Furniture",
+    label: "Furniture",
     image: "https://i.postimg.cc/6qwKgNKv/Generated-Image-7.jpg",
   },
   {
-    category: "Bedroom",
+    label: "Bedroom",
     image: "https://i.postimg.cc/W1tvQnBX/Generated-Image-8.jpg",
   },
   {
-    category: "Outdoor",
+    label: "Outdoor",
     image: "https://i.postimg.cc/TPDXPPYb/Generated-Image-9.jpg",
   },
   {
-    category: "Kitchen",
+    label: "Kitchen",
     image: "https://i.postimg.cc/6p8K0FNf/knife.webp",
   },
   {
-    category: "Sports",
+    label: "Sports",
     image: "https://i.postimg.cc/8CbZrPnK/shoes.webp",
   },
   {
-    category: "Books",
+    label: "Books",
     image:
       "https://i.postimg.cc/gj6hZY52/WhatsApp_Image_2025-01-16_at_23.37.34.jpg",
   },
   {
-    category: "Tools",
+    label: "Tools",
     image: "https://i.postimg.cc/ZRnKjmTK/Generated-Image-4.jpg",
   },
 ];
