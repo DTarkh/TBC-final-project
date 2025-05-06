bc7dbe2c58735dbc6631ab46c070a6c8bdcf3fdf
David Tarkhnishvili <datotarkh@gmail.com>
2025-05-06 15:06:31 +0400

    Changed order of Header in blog details page

--- app/[locale]/(dashboard)/blog/[id]/page.tsx
+++ app/[locale]/(dashboard)/blog/[id]/page.tsx
@@ -1,8 +1,6 @@
-
 import { Post } from "../page";
 import AddComment from "@/app/[locale]/Components/AddComment";
 import CommentSection from "@/app/[locale]/Components/CommentSection";
-
 
 const BlogDetails = async ({ params }: { params: Promise<{ id: number }> }) => {
   const resolvedParams = await params;
@@ -13,7 +11,6 @@ const BlogDetails = async ({ params }: { params: Promise<{ id: number }> }) => {
   );
   const post: Post[] = await response.json();
 
-
   return (
     <div className="flex justify-center w-full">
       {post.map((post) => (
@@ -21,26 +18,27 @@ const BlogDetails = async ({ params }: { params: Promise<{ id: number }> }) => {
           key={post.id}
           className="p-6 bg-white rounded-lg shadow-md w-full max-lg:mx-[2%]"
         >
+          <div className="flex justify-between items-center text-sm text-gray-500 mb-6">
+            <span>By User: {post.user_email}</span>
+            {new Intl.DateTimeFormat("en-US", {
+              timeZone: "Asia/Tbilisi",
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            }).format(new Date(post.created_at))}
+          </div>
           <h1 className="text-2xl font-bold text-gray-800 mb-4">
             {post.title}
           </h1>
 
-          <div className="flex justify-between items-center text-sm text-gray-500 mb-6">
-            <span>By User: {post.user_email}</span>
-            {new Intl.DateTimeFormat("en-US", {
-                  timeZone: 'Asia/Tbilisi',
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: false,
-                }).format(new Date(post.created_at))}
-          </div>
-
-          <p className="text-gray-700 leading-relaxed whitespace-pre-wrap w-full">{post.body}</p>
+          <p className="text-gray-700 leading-relaxed whitespace-pre-wrap w-full">
+            {post.body}
+          </p>
           <CommentSection postID={post.id} />
-          <AddComment postID={post.id}/>
+          <AddComment postID={post.id} />
         </div>
       ))}
     </div>
