0ce16b200b6c9de7b3a05aa62a9f6f86f8ceda5e
David Tarkhnishvili <datotarkh@gmail.com>
2025-05-15 08:09:54 +0400

    Updated global files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#14213D',
+        secondary: '#FCA311',
+        neutral: '#E5E5E5',
+        accent: '#ff9900',
+      },
+      fontFamily: {
+        sans: ['var(--font-suse)', 'system-ui', 'sans-serif'],
+        heading: ['var(--font-suse)', 'system-ui', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        'custom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
       },
     },
   },
